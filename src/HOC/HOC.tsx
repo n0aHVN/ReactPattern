d9acdabba1fea8f66bc323a112011629a316477b
n0aHVN <superherodung123@gmail.com>
2025-07-31 10:11:23 +0700

    Create HOC.tsx

---
+++ src/HOC/HOC.tsx
@@ -0,0 +1,8 @@
+export default function HOC() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Higher-Order Component Pattern</h1>
+      <p>This pattern allows you to create components that can enhance other components.</p>
+    </div>
+  )
+}
